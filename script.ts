d2830fd21e5ce7a6e8fa01d73349a2b9c512ac0f
samuba <samuel.z.bach@gmail.com>
2023-08-23 13:02:03 +0200

    fix scraping

--- script.ts
+++ script.ts
@@ -60,7 +60,7 @@ async function createEntry(listName: string, url: string) {
 				const letterboxdFilms: LetterboxdFilm[] = [];
 				$('.poster-container').each((_, el) => {
 					const name = $(el).find('img').attr('alt')!;
-					const letterboxdUrl = 'https://letterboxd.com' + $(el).find('[data-film-slug]').attr('data-film-slug')!;
+					const letterboxdUrl = 'https://letterboxd.com/film/' + $(el).find('[data-film-slug]').attr('data-film-slug')!;
 					const listPosition = Number($(el).find('.list-number').text());
 					console.log({ name, listPosition });
 					letterboxdFilms.push({ name, letterboxdUrl, listPosition });
